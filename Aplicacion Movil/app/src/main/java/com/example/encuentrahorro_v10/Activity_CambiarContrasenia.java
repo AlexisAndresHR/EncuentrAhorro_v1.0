047e0a5db65360a7d5d36e7d8e97752157c3f8ac
Angeles_Octaviano <37378856+angelesOct@users.noreply.github.com>
2019-03-20 22:22:30 -0600

    interfaz de cambio de contraseña y degradado

---
+++ Aplicacion Movil/app/src/main/java/com/example/encuentrahorro_v10/Activity_CambiarContrasenia.java
@@ -0,0 +1,13 @@
+package com.example.encuentrahorro_v10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_CambiarContrasenia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__cambiar_contrasenia);
+    }
+}
