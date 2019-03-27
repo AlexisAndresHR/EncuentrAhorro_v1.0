3ddd794e2c16b9c40ab9dda04cd0d1ef3cc189d7
Angeles_Octaviano <37378856+angelesOct@users.noreply.github.com>
2019-03-27 17:32:30 -0600

    se agrega interfaz de Verificación en app movil

--- Aplicacion Movil/app/src/main/java/com/example/encuentrahorro_v10/Activity_Codigo_Validacion.java
+++ Aplicacion Movil/app/src/main/java/com/example/encuentrahorro_v10/Activity_Codigo_Validacion.java
@@ -3,11 +3,11 @@ package com.example.encuentrahorro_v10;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Activity_CambiarEmail extends AppCompatActivity {
+public class Activity_Codigo_Validacion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__cambiar_email);
+        setContentView(R.layout.activity__codigo__validacion);
     }
 }
