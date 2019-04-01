ad3e73e18e5f2db5406e654fc7a1a081b1ce117d
AlexisAndresHR <1717110253@utectulancingo.edu.mx>
2019-03-31 21:11:31 -0600

    Avance en las funciones de interfaz Publicar Recomendación (Obtención de datos de ubicación reales y métodos para envío de datos JSON)

--- Aplicacion Movil/app/src/main/java/com/example/encuentrahorro_v10/Activity_PublicarRecomendacion.java
+++ Aplicacion Movil/app/src/main/java/com/example/encuentrahorro_v10/Activity_PublicarRecomendacion.java
@@ -5,13 +5,145 @@ import android.os.Bundle;
 import android.content.Intent; // Import de prueba para cambio de interfaz (temporal)...
 import android.view.View; // Import de prueba para cambio de interfaz (temporal)...
 
-public class Activity_PublicarRecomendacion extends AppCompatActivity {
+// Imports para el envio de informacion mediante JSON
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+// Imports para las funciones de ubicación en la interfaz
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+// Imports para el mapa con marca (Google Maps)
+// Imports para la obtencion de datos de ubicacion del usuario
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
+public class Activity_PublicarRecomendacion extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    EditText et_id_categoria;
+    EditText et_id_producto;
+
+    Spinner sp_categoria;
+    Spinner sp_tipoproducto;
+    EditText et_precio;
+    EditText et_descripcion;
+    Spinner sp_duracion;
+
+    EditText et_latitud;
+    EditText et_longitud;
+
+    private String webservice_url = "http://webapp-encuentrahorro.herokuapp.com" +
+            "./api_recomendaciones?user_hash=dc243fdf1a24cbced74db81708b30788&action=put&";
+
+
+    EditText et_nombre_usuario;
+    EditText et_num_megusta;
+    EditText et_num_comentarios;
+    EditText et_promedio_eva;
+    EditText et_status;
+
+
+    // Variables para las funciones de ubicacion del usuario
+    private static final String TAG = Activity_PublicarRecomendacion.class.getName();
+
+    //private TextView mTvLatitud;
+    //private TextView mTvLongitud;
+
+    private static final int RC_LOCATION_PERMISION= 100;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private static int INTERVAL = 10000;
+    private static int FAST_INTERVAL = 5000;
+
+    private boolean mRequestingLocationUpdates = false;
+    private Location mCurrentLocation;
+
+    // Variables de prueba para almacenar latitud y longitud
+    private double latitud_ubi = 0.0;
+    private double longitud_ubi = 0.0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__publicar_recomendacion);
+        // Inicialización de componentes de la vista
+        sp_categoria = findViewById(R.id.sp_categoria);
+        sp_tipoproducto = findViewById(R.id.sp_tipoproducto);
+        et_precio = findViewById(R.id.et_precio);
+        et_descripcion = findViewById(R.id.et_descripcion);
+        sp_duracion = findViewById(R.id.sp_duracion);
+/*
+        et_latitud = findViewById(R.id.et_latitud);
+        et_longitud = findViewById(R.id.et_longitud);
+        et_id_categoria = findViewById(R.id.et_id_categoria);
+        et_id_producto = findViewById(R.id.et_id_producto);
+        et_nombre_usuario = findViewById(R.id.et_nombre_usuario);
+        et_num_megusta = findViewById(R.id.et_num_megusta);
+        et_num_comentarios = findViewById(R.id.et_num_comentarios);
+        et_promedio_eva = findViewById(R.id.et_promedio_eva);
+        et_status = findViewById(R.id.et_Status);
+*/
+        //Conectar el UI con la Actividad
+        //mTvLatitud = (TextView) findViewById(R.id.latitud);
+        //mTvLongitud= (TextView) findViewById(R.id.longitud);
+
+        //Solicitar permisos si es necesario (Android 6.0+)
+        requestPermissionIfNeedIt();
+        //Inicializar el GoogleAPIClient y armar la Petición de Ubicación
+        initGoogleAPIClient();
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_ubicacion);
+        mapFragment.getMapAsync(this);
     }
+
 
     // Código para prueba de cambio de interfaz (temporal)...
     public void cambioInterfaz2(View view) {
@@ -19,4 +151,233 @@ public class Activity_PublicarRecomendacion extends AppCompatActivity {
         startActivity(cambio2);
     }
 
-}
+
+// Métodos para el envío de información al webservice (Recomendacion en JSON).
+    public void btn_insertOnClick(View view){
+        StringBuilder sb = new StringBuilder();
+        sb.append(webservice_url);
+        sb.append("descripcion="+et_descripcion.getText());
+        sb.append("&");
+        sb.append("precio="+et_precio.getText());
+        sb.append("&");
+        sb.append("latitud_ubi=0.0");
+        sb.append("&");
+        sb.append("longitud_ubi=0.0");
+        sb.append("&");
+        sb.append("duracion=7");
+        sb.append("&");
+        sb.append("id_categoria=1");
+        sb.append("&");
+        sb.append("id_producto=1");
+        sb.append("&");
+        sb.append("nombre_usuario=AlexisHR");
+        sb.append("&");
+        sb.append("num_megusta=0");
+        sb.append("&");
+        sb.append("num_comentarios=0");
+        sb.append("&");
+        sb.append("promedio_evaluaciones=0.0");
+        sb.append("&");
+        sb.append("recomendacion_activa=1");
+        webServicePut(sb.toString());
+        Log.e("URL",sb.toString());
+    }
+    private void webServicePut(String requestURL){
+        try{
+            URL url = new URL(requestURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line = "";
+            String webServiceResult="";
+            while ((line = bufferedReader.readLine()) != null){
+                webServiceResult += line;
+            }
+            bufferedReader.close();
+            parseInformation(webServiceResult);
+        }catch(Exception e){
+            Log.e("Error 100",e.getMessage());
+        }
+    }
+
+    private void parseInformation(String jsonResult){
+        JSONArray jsonArray = null;
+        String status;
+        String description;
+        try{
+            jsonArray = new JSONArray(jsonResult);
+        }catch (JSONException e){
+            Log.e("Error 101",e.getMessage());
+        }
+        for(int i=0;i<jsonArray.length();i++){
+            try{
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                //Se obtiene cada uno de los datos cliente del webservice
+                status = jsonObject.getString("status");
+                description = jsonObject.getString("description");
+                Log.e("STATUS",status);
+                Log.e("DESCRIPTION",description);
+            }catch (JSONException e){
+                Log.e("Error 102",e.getMessage());
+            }
+        }
+    }
+
+
+
+// Métodos para la obtención de ubicación real del usuario (con Google Maps).
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user receives a prompt to install
+     * Play services inside the SupportMapFragment. The API invokes this method after the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(latitud_ubi, longitud_ubi);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+    // Métodos para  las funciones de obtencion de ubicacion del usuario
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            if (mGoogleApiClient.isConnected())
+                startLocationUpdates();
+            else
+                mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopLocationUpdates();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void initGoogleAPIClient(){
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+
+            //Creamos una peticion de ubicacion con el objeto LocationRequest
+            createLocationRequest();
+        }
+    }
+    protected void createLocationRequest(){
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FAST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+    private void startLocationUpdates(){
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+                mRequestingLocationUpdates = true;
+            }
+        }
+    }
+    private void stopLocationUpdates() {
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected() && mRequestingLocationUpdates){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+    private void requestPermissionIfNeedIt() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, RC_LOCATION_PERMISION);
+        }
+    }
+    public void refreshUI(){
+        if (mCurrentLocation != null) {
+            //mTvLatitud.setText(String.valueOf(mCurrentLocation.getLatitude()));
+            //mTvLongitud.setText(String.valueOf(mCurrentLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RC_LOCATION_PERMISION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates();
+            } else {
+                requestPermissionIfNeedIt();
+            }
+        }
+    }
+
+    /*
+     * Implementación del GoogleApiClient.ConnectionCallbacks
+     * */
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected");
+        if (!mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e(TAG, "onConnectionSuspended");
+        Toast.makeText(this, getString(R.string.app_name), Toast.LENGTH_LONG).show();
+    }
+
+    /*
+     * Implementación del GoogleApiClient.OnConnectionFailedListener
+     * */
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, "onConnectionFailed");
+    }
+
+    /*
+     * Implementación del LocationListener
+     * */
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        refreshUI();
+    }
+
+    /**
+     * Método para obtener y reflejar en el mapa la ubicacion del usuario (al hacer click en el boton).
+     * @param view
+     */
+    public void agregarUbicacion (View view) {
+        latitud_ubi = mCurrentLocation.getLatitude();
+        longitud_ubi = mCurrentLocation.getLongitude();
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_ubicacion);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+} // Cirre de la clase principal del Activity.
