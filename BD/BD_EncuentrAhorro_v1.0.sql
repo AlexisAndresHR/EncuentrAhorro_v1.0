d67b93b49c1c716c0a32981146b304bddb61a67b
Alexis Andrés HR <37376901+AlexisAndresHR@users.noreply.github.com>
2019-12-01 19:05:28 -0600

    Modificación en estructura de la BD para nuevas funciones (script sql de estructura)

--- BD/BD_EncuentrAhorro_v1.0.sql
+++ BD/BD_EncuentrAhorro_v1.0.sql
@@ -3,13 +3,13 @@ USE encuentrahorro_v1;
 
 
 CREATE TABLE IF NOT EXISTS usuarios (
-    nombre_usuario varchar(20) primary key,
-    email_usuario varchar(50) not null,
+    nombre_usuario varchar(30) primary key,
+    email_usuario varchar(50) unique not null,
     contrasena_usuario varchar(20) not null,
     nombre varchar(30) not null,
     apellido_pat varchar(30) not null,
     apellido_mat varchar(30) not null,
-    fotografia_usuario blob,
+    fotografia_usuario varchar(200),
     promedio_evaluaciones float not null default 0,
     nivel_usuario varchar(20) not null default "Promedio",
     codigo_recuperacion varchar(20) null
@@ -19,6 +19,22 @@ DESCRIBE usuarios;
 INSERT INTO usuarios (nombre_usuario, email_usuario, contrasena_usuario, nombre, apellido_pat, apellido_mat, codigo_recuperacion) VALUES 
     ('AlexisHR', 'alexisandres006@gmail.com', 'password1', 'Alexis Andrés', 'Hernández', 'Ramírez', ''),
     ('Angie11', '1717110255@utectulancingo.edu.mx', 'password2', 'Maria de los Angeles', 'Gayosso', 'Octaviano', '');
+
+
+
+CREATE TABLE IF NOT EXISTS tiendas (
+    id_tienda int auto_increment primary key,
+    nombre_tienda varchar(50) not null,
+    nom_acceso_tienda varchar(50) not null,
+    contrasena_tienda varchar(20) not null,
+    fotografia_tienda varchar(200),
+    promedio_evaluaciones float not null default 0
+);
+DESCRIBE tiendas;
+
+INSERT INTO tiendas (nombre_tienda, nom_acceso_tienda, contrasena_tienda) VALUES 
+    ('Semillas Aguilar', 'tienda_semillasaguilar', 'registrada01');
+
 
 
 CREATE TABLE IF NOT EXISTS categorias_productos (
@@ -35,7 +51,7 @@ INSERT INTO categorias_productos (nombre_categoria) VALUES
 CREATE TABLE IF NOT EXISTS tipos_productos (
     id_producto int auto_increment primary key,
     nombre_producto varchar(40) not null,
-    imagen_producto blob,
+    imagen_producto varchar(200),
     id_categoria int,
     foreign key (id_categoria) references categorias_productos(id_categoria)
 );
@@ -56,32 +72,33 @@ CREATE TABLE IF NOT EXISTS recomendaciones (
     latitud_ubi varchar(20),
     longitud_ubi varchar(20),
     duracion int not null,
-    id_categoria int,
+    id_categoria int not null,
     id_producto int,
     nombre_usuario varchar(20),
-    num_megusta int not null default 0,
+    id_tienda int,
+    rec_confiable int not null default 0,
+    rec_falsa int not null default 0,
     num_comentarios int not null default 0,
     promedio_evaluaciones float not null default 0,
     recomendacion_activa boolean not null default true,
-    foreign key (id_categoria) references categorias_productos(id_categoria),
     foreign key (id_producto) references tipos_productos(id_producto),
-    foreign key (nombre_usuario) references usuarios(nombre_usuario)
+    foreign key (nombre_usuario) references usuarios(nombre_usuario),
+    foreign key (id_tienda) references tiendas(id_tienda)
 );
 DESCRIBE recomendaciones;
 
 INSERT INTO recomendaciones (descripcion, precio, latitud_ubi, longitud_ubi, duracion, id_categoria, id_producto, nombre_usuario) VALUES 
-	('Litro de gasolina magna.', 17.68, '20.074047', '-98.351546', 2, 1, 2, 'AlexisHR'),
+	('Litro de gasolina magna.', 18.68, '20.074047', '-98.351546', 2, 1, 2, 'AlexisHR'),
 	('250 gramos de arándanos deshidratados.', 67, '20.082488', '-98.355372', 7, 2, 4, 'Angie11');
 
 
 CREATE TABLE IF NOT EXISTS comentarios (
     id_comentario int auto_increment primary key,
     id_recomendacion int,
-    nombre_usuario varchar(20),
-    contenido varchar(200) not null,
+    nombre_usuario varchar(20) not null,
+    contenido varchar(300) not null,
     fecha_comentario timestamp default now(),
-    foreign key (id_recomendacion) references recomendaciones(id_recomendacion),
-    foreign key (nombre_usuario) references usuarios(nombre_usuario)
+    foreign key (id_recomendacion) references recomendaciones(id_recomendacion)
 );
 DESCRIBE comentarios;
 
@@ -92,9 +109,8 @@ INSERT INTO comentarios (id_recomendacion, nombre_usuario, contenido) VALUES
 CREATE TABLE IF NOT EXISTS favoritos (
     id_favorito int auto_increment primary key,
     nombre_usuario varchar(20),
-    id_recomendacion int,
-    foreign key (nombre_usuario) references usuarios(nombre_usuario),
-    foreign key (id_recomendacion) references recomendaciones(id_recomendacion)
+    id_recomendacion int not null,
+    foreign key (nombre_usuario) references usuarios(nombre_usuario)
 );
 DESCRIBE favoritos;
 
@@ -105,13 +121,13 @@ INSERT INTO favoritos (nombre_usuario, id_recomendacion) VALUES
 CREATE TABLE IF NOT EXISTS evaluaciones (
 	id_evaluacion int auto_increment primary key,
 	calificacion int not null,
-	id_recomendacion int,
+	id_tienda int,
 	nombre_usuario varchar(20),
-	foreign key (id_recomendacion) references recomendaciones(id_recomendacion),
+	foreign key (id_tienda) references tiendas(id_tienda),
     foreign key (nombre_usuario) references usuarios(nombre_usuario)
 );
 DESCRIBE evaluaciones;
 
-INSERT INTO evaluaciones (calificacion, id_recomendacion, nombre_usuario) VALUES 
-	(10, 1, 'Angie11');
+INSERT INTO evaluaciones (calificacion, nombre_usuario) VALUES 
+	(5, 'Angie11');
 
